6626df5f8a5cfd0a7171ed4824d9fa1b6e1cd8cf
Oscar Sun <oscar258254@gmail.com>
2025-09-17 13:35:19 -0600

    los datos de reto

--- Carga de datos con Pandas.py
+++ Carga de datos con Pandas.py
@@ -1,9 +1,14 @@
+# IMPORTAR LIBRERIAS
 import pandas as pd
-
-df = pd.read_csv("C:/Users/Oscar/codigo/retoAnalitica/titanic.csv")
-
+# lee el archivo CSV
+df = pd.read_csv("C:/Users/Oscar/codigo/tarea/titanic.csv")
+# muestra las primeras filas del DataFrame
 df.head()
-
+# Con la manera de texto
 print(df.head())
-
+# Descripción estadística del DF
 df.describe()
+# Con manera de texto
+print(df.describe)
+
+
